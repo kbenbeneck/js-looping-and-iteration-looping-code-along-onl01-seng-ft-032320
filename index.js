e6814f50b10c3c6df0e12c1bfb2ecb1523631dff
KBENBENECK <kbenbeneck@gmail.com>
2020-07-02 10:38:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+function writeCards(namesArray, event) {
+    let thanksYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thanksYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    
+    return thanksYouCards
+}
+
+function countDown(anyPositiveInteger){
+    while (anyPositiveInteger > 0) {
+        console.log(anyPositiveInteger);
+        anyPositiveInteger -=1;
+    }
+    console.log(anyPositiveInteger);
+}
